e1bf019264b7176b4000d5ebfda2c97a5a72e7ad
Cedric Geissmann <cedric.geissmann@gmail.com>
2022-11-16 14:41:48 +0100

    Only jump on space.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function handleKeyPress(ev) {
-    const player = document.querySelector("#player")
-    player.setAttribute("cx", 100 * Math.random())
-    player.setAttribute("cy", 100 * Math.random())
+  if (ev.code === "Space") {
+    const player = document.querySelector("#player");
+    player.setAttribute("cx", 100 * Math.random());
+    player.setAttribute("cy", 100 * Math.random());
+  }
 }
 
-
-window.onkeydown = handleKeyPress
+window.onkeydown = handleKeyPress;

2a9bc17560f397a3b7c37219bbb5bb66c9741302
Cedric Geissmann <cedric.geissmann@gmail.com>
2022-11-18 08:38:55 +0100

    handle left right movement

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let dy = 0
+let dx = 0
 
 
 function handleKeyPress(ev) {
@@ -10,6 +11,10 @@ function handleKeyPress(ev) {
     player.setAttribute("fill", "red");
   } else if (ev.key === "g") {
     player.setAttribute("fill", "green");
+  } else if (ev.code === "ArrowRight") {
+    dx = 1
+  } else if (ev.code === "ArrowLeft") {
+    dx = -1
   }
 }
 
@@ -18,13 +23,20 @@ function gameLoop() {
   dy = Math.min(dy + 0.05, 1)
 
   const player = document.querySelector("#player");
+
+
   let posY = parseFloat(player.getAttribute("cy"));
   let r = parseInt(player.getAttribute("r"))
   if (posY > 100 - r - dy) {
     dy = 0
   }
-
   player.setAttribute("cy", posY + dy);
+  
+  
+  let posX = parseFloat(player.getAttribute("cx"));
+  player.setAttribute("cx", posX + dx);
+
+
 
   // Loop forever
   window.requestAnimationFrame(gameLoop);

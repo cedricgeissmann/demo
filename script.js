098e92e4e224b5a1fb2ab49932857223f9963e92
Cedric Geissmann <cedric.geissmann@gmail.com>
2022-11-18 08:10:04 +0100

    Added game loop

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 function handleKeyPress(ev) {
   const player = document.querySelector("#player");
-  console.log(ev)
+  console.log(ev);
   if (ev.code === "Space") {
     player.setAttribute("cx", 100 * Math.random());
     player.setAttribute("cy", 100 * Math.random());
   } else if (ev.key === "r") {
-    player.setAttribute("fill", "red")
+    player.setAttribute("fill", "red");
   } else if (ev.key === "g") {
-    player.setAttribute("fill", "green")
+    player.setAttribute("fill", "green");
   }
 }
+
+function gameLoop() {
+  const player = document.querySelector("#player");
+  player.setAttribute("cy", 100 * Math.random());
+
+  // Loop forever
+  window.requestAnimationFrame(gameLoop);
+}
+
+gameLoop()
 
 window.onkeydown = handleKeyPress;
